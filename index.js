b7e2bbb4ef4b8f6de08ac598c06887eec4c148ce
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-07-15 19:26:19 +0200

    feat: Broke mapNodeResult into mapResult and mapNodeResult to abstract root object mapping operation.
    # Updated docs of the util functions
    # Introduced new small util fns

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { mapRelation } from './lib/mapRelation';
+import { mapRelation } from './lib/mapping/mapRelation';
 
 require('dotenv').config();
 import neo4j from 'neo4j-driver';
 
-import mapNodeResult from './lib/mapNodeResult';
+import mapNodeResult from './lib/mapping/mapNodeResult';
 
 (async () => {
   const driver = neo4j.driver(process.env.NEO4J_URL, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD));
@@ -12,7 +12,13 @@ import mapNodeResult from './lib/mapNodeResult';
   try {
 
     const result = await session.run(
-      'MATCH (p:Person)-[r:DIRECTED]->(m:Movie) RETURN m,p LIMIT 2'
+      `
+        MATCH (p:Person)-[r:DIRECTED]->(m:Movie)
+        MATCH (p)-[:PRODUCED]->(m)
+        RETURN
+            p,
+            collect(distinct {Title:m.title,Tagline:m.tagline}) as m
+      `
     );
 
     return mapNodeResult('p', result.records, [
@@ -20,7 +26,7 @@ import mapNodeResult from './lib/mapNodeResult';
           'm',
           [
             'movie',
-            (item) => ({ tagline: item.tagline, title: item.title }),
+            (item) => ({ tagline: item.Tagline, title: item.Title }),
           ],
         ),
       ]
@@ -29,7 +35,7 @@ import mapNodeResult from './lib/mapNodeResult';
     await session.close();
     await driver.close();
   }
-})().then(console.log);
+})().then(JSON.stringify).then(console.log);
 
 /*
 * Result would be something like this:
